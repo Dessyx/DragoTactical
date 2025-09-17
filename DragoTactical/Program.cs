b8bde405dc9934f8a2f57c5567ed8cb9b9555c2b
bulelacool <bulelacool@gmail.com>
2025-09-17 19:09:19 +0200

    Add DbContext support for DragoTactical
    This commit introduces using directives for `DragoTactical.Models` and `Microsoft.EntityFrameworkCore`. It also adds the `DragoTacticalDbContext` to the service container, enabling database interactions with Entity Framework Core.

--- DragoTactical/Program.cs
+++ DragoTactical/Program.cs
@@ -1,7 +1,11 @@
+using DragoTactical.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DragoTacticalDbContext>();
 
 var app = builder.Build();
 
